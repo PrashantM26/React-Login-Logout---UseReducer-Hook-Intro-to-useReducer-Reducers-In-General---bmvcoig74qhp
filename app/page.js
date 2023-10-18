5998c1a45bfbf9cd411c8b54d02e999eb7c22ab5
PrashantM26 <135729171+PrashantM26@users.noreply.github.com>
2023-10-18 19:46:35 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,40 +1,3 @@
-/*import React from 'react'
-
-const reducer = (state, action) => {
-
-}
-
-function Home() {
-  return (
-    <div id="main">
-      <section className='logout-section'>
-        <h2>Logged in successfully!</h2>
-        <p>Welcome username!</p>
-        <button className='logout-btn'>Logout</button>
-      </section>
-      <form className='login-form'>
-        {/* <p className='invalid-error'>Invalid username or password!</p> *}
-        <section className='username-input'>
-          <label>Username: </label>
-          <input type="text" placeholder='Username' className='username' />
-        </section>
-        <section className='password-input'>
-          <label>Password: </label>
-          <input type="password" placeholder='Password' className='password' />
-        </section>
-        <button className='login-btn'>Login</button>
-      </form>
-    </div>
-  )
-}
-
-export default Home*/
-
-
-
-
-
-//import { useClient } from 'next/react';
 'use client'
 import React, { useReducer } from 'react';
 
@@ -118,4 +81,3 @@ function Home() {
 }
 
 export default Home;
-//export default useClient(Home);
